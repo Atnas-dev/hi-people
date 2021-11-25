03f67093326ee5ba22e3316f662ffec8d7179349
Claus Marin Kaiser <ck@atnas.io>
2021-11-25 18:32:32 -0500

    Bind searchfield result to imageGallery with Unsplash

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { FC, useState } from 'react';
+import { createApi } from 'unsplash-js';
 import { ImageGalleryImage } from './components/imageGallery/@ImageGallery';
 import ImageGallery from './components/imageGallery/ImageGallery';
 import InputString from './components/inputString/InputString';
@@ -7,6 +8,40 @@ import Title from './components/title/Title';
 
 const App: FC = () => {
 	const [images, setImages] = useState<ImageGalleryImage[]>([]);
+	
+	const unsplash = createApi({
+		accessKey: process.env.REACT_APP_UNSPLASH as string,
+	});
+	
+	let searchDebounce: NodeJS.Timeout;
+	const handleSearchChange = (val: string) => {
+		if (searchDebounce) {
+			clearTimeout(searchDebounce);
+		}
+		
+		searchDebounce = setTimeout(() => {
+			unsplash.search.getPhotos({
+				query: val,
+				page: 1,
+				perPage: 50,
+			}).then((res) => {
+				const newImages: ImageGalleryImage[] = [];
+				res.response?.results.forEach((image) => {
+					newImages.push({
+						id: image.id,
+						src: image.urls.small,
+						srcFull: image.urls.full,
+						height: image.height,
+						width: image.width,
+						description: image.alt_description || image.description || '',
+						downloadLink: image.links.download,
+					});
+				});
+				
+				setImages(newImages);
+			});
+		}, 300);
+	};
 	
 	return (
 		<div>
@@ -18,6 +53,7 @@ const App: FC = () => {
 			<InputString
 				name="search"
 				placeholder="Search for an image..."
+				handleChange={handleSearchChange}
 			/>
 			
 			<ImageGallery images={images} />
